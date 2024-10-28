d9d28d99bf430eca78096f7d90a45f6d089d8e02
Roshini Padmanabha <45605490+roshinip21@users.noreply.github.com>
2024-10-28 00:04:30 -0700

    Added minor text changes Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -18,18 +18,17 @@ function Home2() {
               Let Me <span className="purple"> Introduce </span> Myself
             </h1>
             <p className="home-about-body" style={{textAlign:"left"}}>
-            Born in Bengaluru, India - The Silicon Valley of India, passionate about using tech & contributing to startups that solve modern problems. 
+            Born in Bengaluru, India - The Silicon Valley of India, passionate about tech, solving modern problems & anything IT & Data! 
               I completed my <b className="purple"><i>Bachelor's in Computer Science & Engineering </i> </b> & worked full-time for<b className="purple"><i> 2 years in Tata Consultancy Services.</i></b>
               <br />
               <br /> I am currently pursuing my <i>
-                <b className="purple"> Masters in Information Systems  </b></i> at San Diego State University
-               to strengthen my knowledge of Developing Backend Systems, managing Data for Analytics and improving Business IT processes.
-              
+                <b className="purple"> Masters in Information Systems  </b></i> at San Diego State University.
+                             
               <br />
               <br />
               My field of Interest lies in &nbsp;
               <i>
-                <b className="purple">Database Systems, Data Science, Analytics & Data Engineering involving Cloud services & Project Management. </b>
+                <b className="purple">Data Science & Analytics, ETL & Data Engineering, Cloud based services, Project Management & Consulting. </b>
                 {" "}
                 <b className="purple">
                 
@@ -37,15 +36,15 @@ function Home2() {
               </i> I have worked in the Retail & Healthcare domains, and would love to join organizations with similar interests.
               <br />
               <br />
-              In addition I learn by working on projects
-              with <i> <b className="purple">
+             I learn by working on projects
+              leveraging <i> <b className="purple">
                   {" "}
-                  Python Libraries & Frameworks
+                  Datasets, Python Libraries & Frameworks
                 </b>
               </i>
               &nbsp; , and
               
-              explore trending tools for <i><b className="purple">Software Development and Data science.  <br></br>
+              explore trending tools for <i><b className="purple"> ML & Software Development.  <br></br>
                 </b> 
               </i><br></br> On a personal note, my interests include: 
               

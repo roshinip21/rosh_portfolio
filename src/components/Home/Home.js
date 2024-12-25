7b79fdb721521fb00616effa2d05dda3b9084354
Roshini Padmanabha <45605490+roshinip21@users.noreply.github.com>
2024-12-24 18:33:24 -0800

    Update Home.js
    reduced to h2

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,14 +13,14 @@ function Home() {
         <Container className="home-content">
           <Row>
             <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
+              <h2 style={{ paddingBottom: 15 }} className="heading">
                 Hey there!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
                 </span>
               </h1>
 
-              <h1 className="heading-name">
+              <h2 className="heading-name">
                 I'm 
                 <strong className="main-name"> Roshini Padmanabha</strong>
               </h1>
